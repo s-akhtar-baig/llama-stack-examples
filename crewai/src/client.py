71e4fc140471f11ba84df6b457478c88520194b7
Shabana Baig <43451943+s-akhtar-baig@users.noreply.github.com>
2025-09-22 14:12:27 -0400

    Create a sample researcher agent

--- crewai/src/client.py
+++ crewai/src/client.py
@@ -2,7 +2,7 @@
 # This file provides a sample client that connects to a local Llama Stack instance
 #
 
-from crewai.llm import LLM
+from crewai import LLM, Agent, Task, Crew
 
 llm = LLM(
     model="meta_llama/ollama/llama3.2:3b",
@@ -15,6 +15,29 @@ messages = [
     {"role": "user", "content": "Write a two sentence poem about CrewAI."}
 ]
 
+print("Testing llm call to llama3.2:3b:")
 response = llm.call(messages)
-
 print(response)
+print("-" * 10)
+
+researcher = Agent(
+    role="About LLM",
+    goal="You know everything about LLM.",
+    backstory="""You are a master at LLMs and their safety issues.""",
+    llm=llm,
+)
+
+search = Task(
+    description="Answer the following questions about LLMs: {question}",
+    expected_output="An answer to the question.",
+    agent=researcher,
+)
+
+crew = Crew(agents=[researcher], tasks=[search])
+
+result = crew.kickoff(
+    inputs={"question": "What is the latest on LLM and guardrails?"}
+)
+
+print("Testing crewai agent and task execution:")
+print(result)
